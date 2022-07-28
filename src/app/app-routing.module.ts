6a2f539104c3332c2d1438219888b81fcac9890b
Moises Velasquez <mvelasquez@pictoric.com>
2022-07-28 15:40:01 -0500

    config routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'projects',
+    loadChildren: () =>
+      import('./project/project.module').then((m) => m.ProjectModule),
+  },
+  {
+    path: 'about',
+    loadChildren: () =>
+      import('./about/about.module').then((m) => m.AboutModule),
+  },
+  {
+    path: 'contact',
+    loadChildren: () =>
+      import('./contact/contact.module').then((m) => m.ContactModule),
+  },
+  {
+    path: '**',
+    redirectTo: 'projects',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
